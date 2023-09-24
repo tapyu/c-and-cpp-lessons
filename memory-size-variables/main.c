8c921d1975cd7417953c4175eef054b31c0221ca
tapyu <rubem.engenharia@gmail.com>
2023-09-24 10:05:42 -0300

    memory-size-variables

--- memory-size-variables/main.c
+++ memory-size-variables/main.c
@@ -1,64 +1,82 @@
 #include <stdio.h>
 
 int main() {
-    // Declare a char variable
+    // char variable
     char c = 'a';
     printf("Value of char: %c\n", c);
-    printf("Size of char: %zu bytes\n\n", sizeof(c)); // or sizeof(char)
+    printf("Size of char: %zu bytes\n\n", sizeof(char));
     
-    // Declare a float variable
+    // float variable
     float f = 3.1416f; // put the `f` suffix
     printf("Value of float: %f\n", f);
-    printf("Size of float: %zu bytes\n\n", sizeof(f)); // or sizeof(float)
+    printf("Size of float: %zu bytes\n\n", sizeof(float));
     
-    // Declare a double variable
+    // double variable
     double d = 3.1416;
     printf("Value of double: %lf\n", d);
-    printf("Size of double: %zu bytes\n\n", sizeof(d)); // or sizeof(double)
+    printf("Size of double: %zu bytes\n\n", sizeof(double));
     
-    // Declare a long double variable
+    // long double variable
     long double ld = 3.141592653589793238462643383279502884L; // put the `L` suffix
     printf("Value of long double: %Lf\n", ld);
-    printf("Size of long double: %zu bytes\n\n", sizeof(ld)); // or sizeof(long double)
+    printf("Size of long double: %zu bytes\n\n", sizeof(long double));
 
-    // Declare a integer variable
-    signed int i = 32767; // The maximum value that can be represented in a signed int
-    printf("Value of int: %i\n", i);
-    printf("Size of int: %zu bytes\n\n", sizeof(i)); // or sizeof(int)
+    // signed integer variable
+    signed int i = 10;
+    printf("Value of signed int: %i\n", i);
+    printf("Size of signed int: %zu bytes\n\n", sizeof(signed int));
 
-    // Declare a integer variable (in octal)
+    // signed integer variable (in octal)
     signed int octa_i = 032;
-    printf("Value of octal int: %o\n", octa_i); // // use %i to convert from octal to decimal
-    printf("Size of octal int: %zu bytes\n\n", sizeof(octa_i)); // or sizeof(int)
+    printf("Value of signed int in octal: %o\n", octa_i); // // use %i to convert from octal to decimal
+    printf("Size of octal int: %zu bytes\n\n", sizeof(signed int));
 
-    // Declare a integer variable (in hexadecimal)
+    // signed integer variable (in hexadecimal)
     signed int hex_i = 0x1a;
-    printf("Value of hexadecimal int: %x\n", hex_i); // use %i to convert from hexadecimal to decimal
-    printf("Size of hexadecimal int: %zu bytes\n\n", sizeof(hex_i)); // or sizeof(int)
+    printf("Value of signed int in hexadecimal: %x\n", hex_i); // use %i to convert from hexadecimal to decimal
+    printf("Size of hexadecimal int: %zu bytes\n\n", sizeof(signed int));
 
-    // Declare a short integer variable
-    signed int s = 32767; // The maximum value that can be represented in a signed short
-    printf("Value of short: %hi\n", s);
-    printf("Size of short: %zu bytes\n", sizeof(s)); // or sizeof(short)
-    // Attempt to exceed the range of a short
-    s = s + 1; // This will result in numeric overflow
+    // unsigned integer variable
+    unsigned int ui = 10u; // put the `u` suffix
+    printf("Value of unsigned int: %u\n", ui);
+    printf("Size of unsigned int: %zu bytes\n\n", sizeof(unsigned int));
+
+    // signed short integer variable
+    signed short int s = 32767; // The maximum value that can be represented in a signed short
+    printf("Value of signed short int: %hi\n", s);
+    printf("Size of signed short int: %zu bytes\n", sizeof(signed short int));
+    s = s + 1; // attempting to exceed the range results in numeric overflow
     printf("Value of short after overflow: %i\n\n", s);
 
-    // Declare a long integer variable
+    // unsigned short integer variable
+    unsigned short int us = 10;
+    printf("Value of unsigned short int: %hu\n", us);
+    printf("Size of unsigned short int: %zu bytes\n\n", sizeof(unsigned short int));
+
+    // signed long integer variable
     signed long int l = 1234567890l; // put the `l` suffix
-    printf("Value of long: %li\n", l);
-    printf("Size of long: %zu bytes\n\n", sizeof(long)); // or sizeof(long)
+    printf("Value of signed long int: %li\n", l);
+    printf("Size of signed long int: %zu bytes\n\n", sizeof(signed long int));
+
+    // unsigned long integer variable
+    unsigned long int ul = 1234567890ul; // put the `ul` suffix
+    printf("Value of unsigned long int: %lu\n", ul);
+    printf("Size of unsigned long int: %zu bytes\n\n", sizeof(unsigned long int));
     
-    // Declare a long long integer variable
-    long long ll = 1234567890123456789LL;
-    printf("Value of long long: %lld\n", ll);
-    printf("Size of long long: %zu bytes\n\n", sizeof(long long)); // or sizeof(long long)
+    // signed long long integer variable
+    signed long long int ll = 1234567890123456789ll; // put the `ll` suffix
+    printf("Value of signed long long int: %lli\n", ll);
+    printf("Size of signed long long int: %zu bytes\n\n", sizeof(signed long long int));
+
+    // unsigned long long integer variable
+    unsigned long long int ull = 1234567890123456789ull; // put the `ull` suffix
+    printf("Value of unsigned long long int: %llu\n", ull);
+    printf("Size of unsigned long long int: %zu bytes\n\n", sizeof(unsigned long long int));
 
     // Declare size_t variable
     size_t size = 42;
-    // Print the stored value of 'size' and its memory size
-    printf("Value of 'size': %zu\n", size);
-    printf("Memory size of 'size': %zu bytes\n", sizeof(size));
+    printf("Value of size_t: %zu\n", size);
+    printf("Memory size of size_t: %zu bytes\n", sizeof(size));
 
     return 0;
 }
