419a1d78116df7a4eec5463b68abc87c4d417a18
tapyu <rubem.engenharia@gmail.com>
2023-09-25 17:14:15 -0300

    fix char-strings-pointers/strings_vs_char_pointers.c

--- char-strings-pointers/strings_vs_char_pointers.c
+++ char-strings-pointers/strings_vs_char_pointers.c
@@ -25,31 +25,7 @@ int main(void)
     printf("str:     %p\n", str);  // same as `&str[0]`
     printf("pstr:    %p\n", pstr); // same as `&str[0]`
     printf("&c:      %p\n", &c);
-    printf("str2:    %p\n\n", str2);
+    printf("str2:    %p\n", str2);
 
-    // dereference via indexing
-    printf("Dereference via indexing:");
-    printf("str[1]:   %c\n",   str[1]);
-    printf("pstr[1]:  %c\n",   pstr[1]);
-    printf("pstr[1]:  %c\n",   pstr[1]);
-    printf("piarr[1]: %i\n\n", piarr[1]);
-
-    // char another_str[strlen(pstr)];
-    // strcpy(another_str, pstr);
-    // printf("Test: \"%c\"\n", pstr[0]);
-    // printf("Test: \"%i\"\n", str);
-    // printf("I can print the sentence by using the str variable: \"%s\"\n", &str[0]);
-    // printf("For some reason, I can printf all the string by using the pointer: \"%s\"\n", pstr);
-    // printf("I can even initialize a new string with this pointer: \"%s\"\n", another_str);
-    // printf("Can I also do it with the int array? %i", piarr);
-
-    
-    // int iarr[] = {55, 10, 12, 66};
-    // int *piarr = iarr;
-
-    // printf("The stored memory addresses:\n"
-    //        "char array=%p\n"
-    //        "int array =%p\n",
-    //        pstr, piarr);
     return 0;
 }
