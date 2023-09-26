794cce7fb00d7221458d587d9a2646b451725252
tapyu <rubem.engenharia@gmail.com>
2023-09-26 20:31:05 -0300

    add move semantics and rvalue reference

--- rvalue-and-move-semantics/main.cpp
+++ rvalue-and-move-semantics/main.cpp
@@ -7,14 +7,14 @@ private:
 
 public:
     // Constructor that takes a C-style string (char*)
-    MyString(char* s) {
+    MyString(const char *s) {
         // Allocate memory and copy the input string
         str = new char[strlen(s) + 1]; // Allocate memory for str with a char array whose length equals `strlen(s) + 1` (for the null character `\0`)
         strcpy(str, s);
     }
 
     // Move constructor using an rvalue reference
-    MyString(MyString&& other) noexcept {
+    MyString(MyString&& other) noexcept { // other is a rvalue reference
         // Steal the ownership of the dynamically allocated memory
         str = other.str;
         other.str = nullptr; // Ensure the source object has no ownership
@@ -36,7 +36,7 @@ int main() {
     MyString temp("Hello, World!");
 
     // Use std::move to cast temp into an rvalue reference
-    MyString moved = std::move(temp);
+    MyString moved = std::move(temp);  // transfer ownership of the dynamically allocated memory from temp to moved
 
     // 'temp' is now in a valid but unspecified state
     // 'moved' has taken ownership of the dynamically allocated memory
@@ -46,6 +46,7 @@ int main() {
 
     // Attempting to print 'temp' would result in undefined behavior
     // because it no longer owns the memory
+    // temp.print();
 
     return 0;
 }
