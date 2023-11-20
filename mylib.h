729b434ccce4a7bf77871af1cbc7ec7ad61458a0
tapyu <rubem.engenharia@gmail.com>
2023-11-20 08:31:29 -0300

    add directory to custom lib

--- mylib.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYLIB_H
-#define MYLIB_H
-
-// leave only one of the following function declarations to use the static or shared lib
-void printMessage(); // function declaration for static lib
-// extern "C" void printMessage(); // function declaration for shared lib
-
-#endif
